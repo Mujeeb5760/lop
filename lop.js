dbcebb896faaafcb3116ed3511c4f8bf801b671c
Mujeeb5760 <108324950+Mujeeb5760@users.noreply.github.com>
2022-08-03 09:32:46 +0500

    add

---
+++ lop.js
@@ -0,0 +1,9 @@
+function generateTable (){
+    let table =document.getElementById('tableValue').value;
+    for (let index = 1 ; index <=10; index++) {
+        document.write (
+            "<h1>" + table + " x" + index + " = " + table * index + "</h1>"
+        );
+        
+    } 
+}
